45e6b7d6f3bf3daa3723f88ae49bbc456b99f4c4
sdiehl <stephen.m.diehl@gmail.com>
2019-12-16 12:17:03 +0000

    Update interpolation example

--- examples/Interpolation.hs
+++ examples/Interpolation.hs
@@ -2,22 +2,20 @@
 
 module Interpolation where
 
-import Protolude
-
 import Data.Curve.Weierstrass.BN254 (Fr)
-import Data.Pairing.BN254           (getRootOfUnity)
-
+import Data.Pairing.BN254 (getRootOfUnity)
 import FFT
+import Protolude
 
 k :: Int
 k = 5
 
 polySize :: Int
-polySize = 2^k
+polySize = 2 ^ k
 
 leftCoeffs, rightCoeffs :: [Fr]
-leftCoeffs = map fromIntegral [1..polySize]
-rightCoeffs = map fromIntegral (reverse [1..polySize])
+leftCoeffs = map fromIntegral [1 .. polySize]
+rightCoeffs = map fromIntegral (reverse [1 .. polySize])
 
 main :: IO ()
 main = do
